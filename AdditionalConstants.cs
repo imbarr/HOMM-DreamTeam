4c89a5da046076bf8e9191a9251013525a10cffd
Илья <@DESKTOP-FUHL82J>
2017-03-31 19:03:07 +0500

    constants

---
+++ AdditionalConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HoMM.ClientClasses;
+
+namespace Homm.Client
+{
+    class AdditionalConstants
+    {
+        public readonly ReadOnlyDictionary<Terrain, double> MovementResistanceMultiplier =
+            new ReadOnlyDictionary<Terrain, double>(new Dictionary<Terrain, double>
+            {
+                {Terrain.Road, 1.0},
+                {Terrain.Desert, 1.5},
+                {Terrain.Grass, 1.3},
+                {Terrain.Snow, 1.7},
+                {Terrain.Marsh, 1.7}
+            });
+    }
+}
