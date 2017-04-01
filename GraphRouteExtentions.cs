f58c2c812517605897f31f843ef15b6c61532553
Артур <cod.hard@yandex.ru>
2017-04-01 17:32:39 +0500

    Артур

--- GraphRouteExtentions.cs
+++ GraphRouteExtentions.cs
@@ -36,19 +36,52 @@ namespace Homm.Client
             }
         }
 
-        public static IEnumerable<Node> FindPathToClosest(Dictionary<Node, NodePathInfo> graphPathInfo, Func<Node, bool> isTarget)
+        public static IEnumerable<Node> FindPathToClosest(Node lastNode, Dictionary<Node, NodePathInfo> graphPathInfo, Func<Node, bool> isTarget)
         {
             NodePathInfo minNodePathInfo = null;
             foreach (var nodePathInfo in graphPathInfo.Where(p => isTarget(p.Key)).Select(p => p.Value))
             {
                 if (nodePathInfo.TravelTime != 0
-                    && (minNodePathInfo == null 
+                    && (minNodePathInfo == null
                     || nodePathInfo.TravelTime < minNodePathInfo.TravelTime))
                 {
                     minNodePathInfo = nodePathInfo;
                 }
             }
+            if (minNodePathInfo == null)//Избегаем Exception если minNodePathInfo.Path пустой, герой просто стоит
+            {
+                Console.WriteLine("Stop");
+                var result = new List<Node>() { lastNode };
+                return result;
+            }
             return minNodePathInfo.Path;
+        }
+        public static IEnumerable<Node> FindSafeNode(Node start)
+        {
+            var visited = new HashSet<Node>();
+            var queue = new Queue<Node>();
+            queue.Enqueue(start);
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                if (node.MapObjectData.NeutralArmy != null)
+                {
+                    visited.Add(node);
+                }
+                if (visited.Contains(node))
+                {
+                    continue;
+                }
+                visited.Add(node);
+                if (node.MapObjectData.ResourcePile != null
+                    || node.MapObjectData.Dwelling != null && node.MapObjectData.Dwelling.Owner != "Left"
+                    || node.MapObjectData.Mine != null && node.MapObjectData.Mine.Owner != "Left")
+                {
+                    yield return node;
+                }
+                foreach (var incidentNode in node.IncidentNodes)
+                    queue.Enqueue(incidentNode);
+            }
         }
     }
 }
--- Homm.Client.Program.cs
+++ Homm.Client.Program.cs
@@ -16,7 +16,7 @@ namespace Homm.Client
         public static void Main(string[] args)
         {
             if (args.Length == 0)
-                args = new[] {"127.0.0.1", "18700"};
+                args = new[] { "127.0.0.1", "18700" };
             var ip = args[0];
             var port = int.Parse(args[1]);
 
@@ -28,27 +28,27 @@ namespace Homm.Client
             var sensorData = client.Configurate(
                 ip, port, CvarcTag,
 
-                timeLimit: 90, // Продолжительность матча в секундах (исключая время, которое "думает" ваша программа). 
+                timeLimit: 120, // Продолжительность матча в секундах (исключая время, которое "думает" ваша программа). 
 
                 operationalTimeLimit: 2000, // Суммарное время в секундах, которое разрешается "думать" вашей программе. 
-                // Вы можете увеличить это время для отладки, чтобы ваш клиент не был отключен, 
-                // пока вы разглядываете программу в режиме дебаггинга.
+                                            // Вы можете увеличить это время для отладки, чтобы ваш клиент не был отключен, 
+                                            // пока вы разглядываете программу в режиме дебаггинга.
 
                 seed: 1,
                 // Seed карты. Используйте этот параметр, чтобы получать одну и ту же карту и отлаживаться на ней.
                 // Иногда меняйте этот параметр, потому что ваш код должен хорошо работать на любой карте.
 
-                spectacularView: true, // Вы можете отключить графон, заменив параметр на false.
+                spectacularView: false, // Вы можете отключить графон, заменив параметр на false.
 
-                debugMap: false,
+                debugMap: true,
                 // Вы можете использовать отладочную простую карту, чтобы лучше понять, как устроен игоровой мир.
 
                 level: HommLevel.Level1, // Здесь можно выбрать уровень. На уровне два на карте присутствует оппонент.
 
                 isOnLeftSide: true
-                // Вы можете указать, с какой стороны будет находиться замок героя при игре на втором уровне.
-                // Помните, что на сервере выбор стороны осуществляется случайным образом, поэтому ваш код
-                // должен работать одинаково хорошо в обоих случаях.
+            // Вы можете указать, с какой стороны будет находиться замок героя при игре на втором уровне.
+            // Помните, что на сервере выбор стороны осуществляется случайным образом, поэтому ваш код
+            // должен работать одинаково хорошо в обоих случаях.
             );
 
             /*var path = new[] { Direction.RightDown, Direction.RightUp, Direction.RightDown, Direction.RightUp, Direction.LeftDown, Direction.Down, Direction.RightDown, Direction.RightDown, Direction.RightUp };
@@ -57,22 +57,54 @@ namespace Homm.Client
                 sensorData = client.Move(e);
             sensorData = client.Move(Direction.RightDown);
             client.Exit();*/
-            
-            for(var i = 1; i <= 30; i++)
+            var graph = new Graph(sensorData.Map);
+            var startNode = graph[sensorData.Location.X, sensorData.Location.Y];
+            var allSafeTarget = GraphRouteExtentions.FindSafeNode(startNode);
+            while (allSafeTarget.Count() != 0)
             {
-                var graph = new Graph(sensorData.Map);
+                graph = new Graph(sensorData.Map);
+                startNode = graph[sensorData.Location.X, sensorData.Location.Y];
                 var gpi = GraphRouteExtentions.GetGraphPathInfo(graph[sensorData.Location.X, sensorData.Location.Y]);
-                var path = GraphRouteExtentions.FindPathToClosest(gpi, n => n.MapObjectData.ResourcePile != null || n.MapObjectData.Dwelling != null && n.MapObjectData.Dwelling.Owner != "Left").Reverse();
+                var path = GraphRouteExtentions.FindPathToClosest(startNode, gpi, n => IsValidTarget(n)).Reverse();
                 var commandsSequence = new List<Direction>();
                 commandsSequence = Convertation.ToDirectionList(path);
                 foreach (var command in commandsSequence)
                 {
                     sensorData = client.Move(command);
                 }
+
+                allSafeTarget = GraphRouteExtentions.FindSafeNode(startNode);
             }
+            //Console.WriteLine("Exit");
+            //var newGpi = GraphRouteExtentions.GetGraphPathInfo(graph[sensorData.Location.X, sensorData.Location.Y]);
+            //var newPath = GraphRouteExtentions.FindPathToClosest(newGpi, n => n.MapObjectData.NeutralArmy != null).Reverse();
+            //var newCommandsSequence = new List<Direction>();
+            //newCommandsSequence = Convertation.ToDirectionList(newPath);
+            //foreach (var command in newCommandsSequence)
+            //{
+            //    sensorData = client.Move(command);
+            //}
+
+            //for(var i = 1; i <= 30; i++)
+            //{
+            //    var graph = new Graph(sensorData.Map);
+            //    var gpi = GraphRouteExtentions.GetGraphPathInfo(graph[sensorData.Location.X, sensorData.Location.Y]);
+            //    var path = GraphRouteExtentions.FindPathToClosest(gpi, n => n.MapObjectData.ResourcePile != null || n.MapObjectData.Dwelling != null && n.MapObjectData.Dwelling.Owner != "Left").Reverse();
+            //    var commandsSequence = new List<Direction>();
+            //    commandsSequence = Convertation.ToDirectionList(path);
+            //    foreach (var command in commandsSequence)
+            //    {
+            //        sensorData = client.Move(command);
+            //    }
+            //}
         }
 
-
+        public static bool IsValidTarget(Node node)
+        {
+            return node.MapObjectData.ResourcePile != null
+                || node.MapObjectData.Dwelling != null && node.MapObjectData.Dwelling.Owner != "Left"
+                || node.MapObjectData.Mine != null && node.MapObjectData.Mine.Owner != "Left";
+        }
         static void Print(HommSensorData data)
         {
             Console.WriteLine("---------------------------------");
